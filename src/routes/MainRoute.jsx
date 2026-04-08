9f4c780efe2b8f3a911aa26df39979aa70212207
epaulino27 <143731967+epaulino27@users.noreply.github.com>
2026-04-08 13:15:09 -0400

    Basic Error Page

--- src/routes/MainRoute.jsx
+++ src/routes/MainRoute.jsx
@@ -8,6 +8,7 @@ import AdminDashboard from "../components/AdminDashboard";
 import AdminJobPage from "../components/AdminJobPage";
 import AdminStatsPage from "../components/AdminStatsPage";
 import AdminUserPage from "../components/AdminUserPage";
+import ErrorPage from "../components/ErrorPage";
 
 const MainRouter = [
   {
@@ -22,10 +23,15 @@ const MainRouter = [
         
     //   { path: "signup", Component: SignUpPage },
     //   { path: "login", Component: LoginPage },
-    //   { path: "error", Component: ErrorPage },
+        { path: "error", Component: ErrorPage },
+        { path: "*", Component: ErrorPage },
         { path: "search", Component: JobFinder },
         { path: "recruiter-dashboard", Component: RecuiterDashboard },
         { path: "job-seeker-dashboard", Component: JSDashboard },
+        { path: "admin-dashboard", Component: AdminDashboard },
+        { path: "admin-jobpage", Component: AdminJobPage },
+        { path: "admin-statspage", Component: AdminStatsPage },
+        { path: "admin-userpage", Component: AdminUserPage },
     ],
   },
 ];
