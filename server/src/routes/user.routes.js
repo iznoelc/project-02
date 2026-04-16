3156b40a72ff3ed6b5cab7fb86133817a9d318c2
iznoelc <izzybellanoel@gmail.com>
2026-04-15 22:52:00 -0400

    import deleteUser

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createUser, getUserByUID } = require("../controllers/user.controller");
+const { createUser, getUserByUID, deleteUser } = require("../controllers/user.controller");
 
 router.post("/", createUser);
 
