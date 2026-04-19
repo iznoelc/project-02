48fbddad7d585df22513d449793d187aa43e0ee6
iznoelc <izzybellanoel@gmail.com>
2026-04-19 13:47:39 -0400

    get searching to work with database on job seeker dashboard

--- client/src/utils/SearchAppsFromJobId.jsx
+++ client/src/utils/SearchAppsFromJobId.jsx
@@ -1,29 +1,37 @@
 function Search(data, searchQuery, searchType){
     // if data, searchQuery, or searchType are null or undefined, return the original data list (or an empty list if data is null)
-    if (!data || !searchQuery || !searchType) return data || [];
+    if (!data) [];
 
     // set what user is searching for to a string and to lowercase for easier searching
-    const value = searchQuery.toString().toLowerCase();
+    const value = searchQuery?.toString().toLowerCase().trim();
+
+    if (!value) return data;
 
     // return list of filtered data based on search query and search type
     // change to lowercase and to string (for the year) to avoid simple grammar msistakes resulting in no results
         return data.filter(item => {
-            switch (searchType) {
-            case "job_title":
-                return item.job_title.toString().toLowerCase().includes(value);
+            const field = item?.[searchType];
 
-            case "location":
-                return item.location.toString().toLowerCase().includes(value);
+            if (!field) return false;
 
-            case "salary":
-                return item.salary_range.toString().toLowerCase().includes(value);
+            return field
+            .toString()
+            .toLowerCase()
+            .includes(value);
+
+            // switch (searchType) {
+            // case "job_title":
+            //     return item.job_title.toString().toLowerCase().includes(value);
+
+            // case "location":
+            //     return item.location.toString().toLowerCase().includes(value);
                 
-            case "category":
-                return item.category.toString().toLowerCase().includes(value);
+            // case "category":
+            //     return item.category.toString().toLowerCase().includes(value);
 
-            default:
-                return true;
-            }
+            // default:
+            //     return true;
+            // }
         });
 }
 
