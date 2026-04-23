6f3fc7304fe5145f66b47d5126ae3a3aef45fa91
iznoelc <izzybellanoel@gmail.com>
2026-04-23 19:33:26 -0400

    db cleanup push

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -7,25 +7,6 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoose = require("mongoose");
 const uri = process.env.MONGO_URI;
 
-const JobPosting = require("../models/jobPosting.model");
-const jobPostingData = require("../data/jobs_100_seed.json");
-
-const Application = require("../models/application.model");
-const applicationData = require("../data/applications_5_seed.json");
-
-const User = require("../models/user.model");
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// let db;
-
 /**
  * If not already connected, connect to the database and return the db instance 
  */
